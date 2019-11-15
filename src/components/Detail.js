31c0893d019e27352c87917a723c0c21e543e857
Phan Quang <phanlequang99@gmail.com>
2019-11-15 14:16:38 +0700

    add router

---
+++ src/components/Detail.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Layout, Menu, Breadcrumb, Icon, Avatar } from "antd";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+
+export default class Detail extends React.Component {
+  render() {
+    return (
+     <h2>Detail</h2>
+    );
+  }
+}

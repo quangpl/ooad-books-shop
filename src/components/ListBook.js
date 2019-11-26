0d3b4446a88a97ebb9bc400516587871d108a43b
phucgo240699 <phucgo240699@gmail.com>
2019-11-26 09:27:10 +0700

    Add book, add list book

---
+++ src/components/ListBook.js
@@ -0,0 +1,24 @@
+import React,{Component} from 'react'
+import Book from './Book.js'
+import '../assets/css/ListBook.css'
+class ListBook extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return (
+            <div class="list_book">
+                <Book image="https://salt.tikicdn.com/cache/550x550/ts/product/35/7e/51/5456c62294cb1f0abdaac4a611a00004.jpg" name="Tại sao thầy nói đúng" author="Richard Wiseman" price="73.261" salePricePercent="30" originalPrice="190.500"></Book>
+                <Book image="https://salt.tikicdn.com/cache/550x550/ts/product/35/7e/51/5456c62294cb1f0abdaac4a611a00004.jpg" name="Tại sao thầy nói đúng" author="Richard Wiseman" price="73.261" salePricePercent="30" originalPrice="190.500"></Book>
+                <Book image="https://salt.tikicdn.com/cache/550x550/ts/product/35/7e/51/5456c62294cb1f0abdaac4a611a00004.jpg" name="Tại sao thầy nói đúng" author="Richard Wiseman" price="73.261" salePricePercent="30" originalPrice="190.500"></Book>
+                <Book image="https://salt.tikicdn.com/cache/550x550/ts/product/35/7e/51/5456c62294cb1f0abdaac4a611a00004.jpg" name="Tại sao thầy nói đúng" author="Richard Wiseman" price="73.261" salePricePercent="30" originalPrice="190.500"></Book>
+                <Book image="https://salt.tikicdn.com/cache/550x550/ts/product/35/7e/51/5456c62294cb1f0abdaac4a611a00004.jpg" name="Tại sao thầy nói đúng" author="Richard Wiseman" price="73.261" salePricePercent="30" originalPrice="190.500"></Book>
+                
+            </div>
+
+        )
+    }
+}
+
+export default ListBook

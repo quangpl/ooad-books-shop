28b95093e34e864a38a88986c31f2e2bd20eef04
Phan Lê Quang <36843088+quangpl@users.noreply.github.com>
2019-12-10 09:23:11 +0700

    Revert "Feature/login"

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,9 @@
 import React from "react";
 import Detail from "./Detail";
 import Home from "./Home";
-import Login from "./Login";
-import Register from "./Register";
 
 import { Layout, Menu, Icon, Avatar } from "antd";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
 const { SubMenu } = Menu;
 const { Header, Content, Footer, Sider } = Layout;
 
@@ -94,17 +91,7 @@ export default class App extends React.Component {
                 <Route exact path="/detail">
                   <Detail />
                 </Route>
-                <Route exact path="/login">
-                  <Login />
-                </Route>
-                <Route exact path="/register">
-                  <Register />
-                </Route>
-
               </Switch>
-
-
-              
             </Content>
             <Footer style={{ textAlign: "center" }}>
               EZBooks @2019 Created by EZGroup

821c3064611de1f8c3b0a233714f5afc7c7b8c5c
phucgo240699 <phucgo240699@gmail.com>
2019-12-10 22:44:26 +0700

    update ui list book

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,6 +1,12 @@
 import React,{Component} from 'react'
 import '../assets/css/Book.css'
 import { Icon } from 'antd'
+
+import {
+    
+    Link
+  } from "react-router-dom";
+
 class Book extends React.Component {
     constructor(props){
         super(props)
@@ -8,24 +14,24 @@ class Book extends React.Component {
     
   render(){
       return(
-        <span class="book">
-            <a>
-                <img src={this.props.image} class="image_book"></img>
-            </a>
-                
-            <figure>
-                <h3>{this.props.name}</h3>
-                <p>{this.props.author}</p>
-                <span>
-                    <strong>{this.props.price}</strong>
+        <div class="book">
+                <Link>  
                     <span class="discount"> -{this.props.salePricePercent}%  </span>
-                    <span class="origin_price"><strike>{this.props.originalPrice}</strike></span>
+                    <img src={this.props.image} class="image_book"></img>
+                </Link>
+
+                <h3>{this.props.name}</h3>
+                
+                <div class="book_info">
+                    <strong class="price">{this.props.price}</strong>
+                    <span class="origin_price"><strike>{this.props.originalPrice}</strike></span> 
+                    
                     <button class="save_button">
-                        <Icon type="heart" class="favorite"></Icon>
+                        <Icon type="heart" class="favorite_icon"></Icon>
                     </button>
-                </span>
-            </figure>   
-        </span>   
+                </div>
+        </div>   
+
       )
   }
 }
--- src/components/ListBook.js
+++ src/components/ListBook.js
@@ -1,4 +1,5 @@
 import React,{Component} from 'react'
+import { Row, Col } from 'antd';
 import Book from './Book.js'
 import '../assets/css/ListBook.css'
 class ListBook extends React.Component{
@@ -9,12 +10,20 @@ class ListBook extends React.Component{
     render(){
         return (
             <div class="list_book">
-                <Book image="https://salt.tikicdn.com/cache/550x550/ts/product/35/7e/51/5456c62294cb1f0abdaac4a611a00004.jpg" name="Tại sao thầy nói đúng" author="Richard Wiseman" price="73.261" salePricePercent="30" originalPrice="190.500"></Book>
-                <Book image="https://salt.tikicdn.com/cache/550x550/ts/product/35/7e/51/5456c62294cb1f0abdaac4a611a00004.jpg" name="Tại sao thầy nói đúng" author="Richard Wiseman" price="73.261" salePricePercent="30" originalPrice="190.500"></Book>
-                <Book image="https://salt.tikicdn.com/cache/550x550/ts/product/35/7e/51/5456c62294cb1f0abdaac4a611a00004.jpg" name="Tại sao thầy nói đúng" author="Richard Wiseman" price="73.261" salePricePercent="30" originalPrice="190.500"></Book>
-                <Book image="https://salt.tikicdn.com/cache/550x550/ts/product/35/7e/51/5456c62294cb1f0abdaac4a611a00004.jpg" name="Tại sao thầy nói đúng" author="Richard Wiseman" price="73.261" salePricePercent="30" originalPrice="190.500"></Book>
-                <Book image="https://salt.tikicdn.com/cache/550x550/ts/product/35/7e/51/5456c62294cb1f0abdaac4a611a00004.jpg" name="Tại sao thầy nói đúng" author="Richard Wiseman" price="73.261" salePricePercent="30" originalPrice="190.500"></Book>
-                
+                <Row>
+                    <Col span={6}>
+                        <Book image="https://salt.tikicdn.com/cache/550x550/ts/product/35/7e/51/5456c62294cb1f0abdaac4a611a00004.jpg" name="Tại sao thầy nói đúng" author="Richard Wiseman" price="73.261" salePricePercent="30" originalPrice="190.500"></Book>
+                    </Col>
+                    <Col span={6}>
+                        <Book image="https://salt.tikicdn.com/cache/550x550/ts/product/35/7e/51/5456c62294cb1f0abdaac4a611a00004.jpg" name="Tại sao thầy nói đúng" author="Richard Wiseman" price="73.261" salePricePercent="30" originalPrice="190.500"></Book>
+                    </Col>
+                    <Col span={6}>
+                        <Book image="https://salt.tikicdn.com/cache/550x550/ts/product/35/7e/51/5456c62294cb1f0abdaac4a611a00004.jpg" name="Tại sao thầy nói đúng" author="Richard Wiseman" price="73.261" salePricePercent="30" originalPrice="190.500"></Book>
+                    </Col>
+                    <Col span={6}>
+                        <Book image="https://salt.tikicdn.com/cache/550x550/ts/product/35/7e/51/5456c62294cb1f0abdaac4a611a00004.jpg" name="Tại sao thầy nói đúng" author="Richard Wiseman" price="73.261" salePricePercent="30" originalPrice="190.500"></Book>
+                    </Col>
+                </Row>
             </div>
 
         )
